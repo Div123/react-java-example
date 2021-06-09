5788dd3cc3549664b402bbe43e491b53e5a7c6e4
Div123 <bdiyas@gmail.com>
2021-06-09 21:29:52 +0530

    React-Java Test

--- src/main/java/com/bbtutorials/users/AppController.java
+++ src/main/java/com/bbtutorials/users/AppController.java
@@ -3,6 +3,8 @@ package com.bbtutorials.users;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bbtutorials.users.controller.UsersController;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 public class AppController {
+	private static final org.slf4j.Logger log = 
+		    org.slf4j.LoggerFactory.getLogger(AppController.class);
 	
 	@RequestMapping({"/"})
 	public String loadUI() {
--- src/main/java/com/bbtutorials/users/controller/UsersController.java
+++ src/main/java/com/bbtutorials/users/controller/UsersController.java
@@ -20,6 +20,8 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/")
 public class UsersController {
+	private static final org.slf4j.Logger log = 
+		    org.slf4j.LoggerFactory.getLogger(UsersController.class);
 	
 	@Autowired
 	UsersService usersService;
